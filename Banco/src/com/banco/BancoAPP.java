e78e7fff73cecaeea64fe991620effcacb6a47e8
Jorge Maykon <jorge.maykonms@gmail.com>
2024-03-20 23:39:11 -0300

    Testing with blackbox and copilot

--- Banco/src/com/banco/BancoAPP.java
+++ Banco/src/com/banco/BancoAPP.java
@@ -1,4 +1,5 @@
-package com.banco;
+/*package com.banco;
+
 import java.awt.font.TextLayout;
 import java.util.Random;
 import java.util.Scanner;
@@ -6,39 +7,40 @@ import javax.swing.*;
 import javax.swing.JOptionPane;
 
 public class BancoAPP extends ContaPF  {
-		public BancoAPP(int numAg, int numConta, double saldo) {
-			super(1, 2, 0.00);
-		}
+	public BancoAPP(int numAg, int numConta, double saldo) {
+		super(1, 2, 0.00);
+	}
 	public static void main(String[] args) {
 
-	Object[] menu = {"Login", "Novo Usuario", "sair"};
-	Object[] opcoes = 	{"Conta PF", "Conta PJ", "Sair"};
-    Object  opcao;
+		Object[] menu = {"Login", "Novo Usuario", "sair"};
+		Object[] opcoes = 	{"Conta PF", "Conta PJ", "Sair"};
+		int opcao;
+		int opcao1;
 
-    Object[] cargos = 	{"Gerente", "Coordenador", "Analista", "Programador"};
-    Object[] empresas = {"Alimenticio", "Transporte"};
-    Object empresa; 
-    Object[] opspf ={"Consultar Dados","Consultar Saldo", "Depositar saldo", "Sacar", "Sair"};
-    Object oppf;
-    
+		Object[] cargos = 	{"Gerente", "Coordenador", "Analista", "Programador"};
+		Object[] empresas = {"Alimenticio", "Transporte"};
+		Object empresa;
+		Object[] opspf ={"Consultar Dados","Consultar Saldo", "Depositar saldo", "Sacar", "Sair"};
+		//Object oppf;
+
 //all instances
-    Random G = new Random();
-	Pessoa User = new Pessoa(01);
-	Form Tela = new Form();
+		Random G = new Random();
+		Pessoa User = new Pessoa(01);
+		Form Tela = new Form();
+
 //menu initial
-		  opcao = JOptionPane.showOptionDialog(null,
-			"Escolha uma opção",
-			"SeuBank",
-			JOptionPane.YES_NO_CANCEL_OPTION,
-			JOptionPane.QUESTION_MESSAGE,
-			null,
-			menu,
-			menu[0]);
+		opcao = JOptionPane.showOptionDialog(null,
+				"Escolha uma opção",
+				"SeuBank",
+				JOptionPane.YES_NO_CANCEL_OPTION,
+				JOptionPane.QUESTION_MESSAGE,
+				null,
+				menu,
+				menu[0]);
 
-		  int opca = Integer.parseInt(opcao.toString());
 
-		  switch (opca){
-			  case 0:
+		switch (opcao){
+			case 0:
 				SwingUtilities.invokeLater(() -> {
 					Tela.setVisible(true);
 					Tela.getJMenuBar();
@@ -49,7 +51,7 @@ public class BancoAPP extends ContaPF  {
 					Tela.getContentPane().add(mensagem);
 					Tela.pack();
 				});
-				  break;
+				break;
 
 
 			case 1:
@@ -64,106 +66,154 @@ public class BancoAPP extends ContaPF  {
 					Tela.pack();
 				});
 				break;
+			case 2:
+				Tela.setVisible(true);
+				Tela.setSize(750, 600);
+				Tela.setTitle("My Bank");
+				JLabel mensagem = new JLabel("Volte sempre!:");
+				Tela.getContentPane().add(mensagem);
+				Tela.pack();
+				System.out.println("volte sempre");
+				break;
 
-		  }
+		}
+	}
+}*/
+
+
+// 	BRACK BOX
+
+/*package com.banco;
+
+import java.awt.font.TextLayout;
+import java.util.Random;
+import javax.swing.*;
+
+public class BancoAPP {
+	// Criação de classes internas para representar diferentes tipos de contas
+	private abstract class Conta{
+		protected int numAg;
+		protected int numConta;
+		protected double saldo;
+
+		public Conta(int numAg, int numConta, double saldo) {
+			this.numAg = numAg;
+			this.numConta = numConta;
+			this.saldo = saldo;
+		}
+
+		public abstract void consultarDados();
+		public abstract void consultarSaldo();
+		public abstract void depositar(double valor);
+		public abstract void sacar(double valor);
+	}
+
+	private class ContaPF extends Conta {
+		private String nome;
+		private String cpf;
+
+		public ContaPF(int numAg, int numConta, double saldo, String nome, String cpf) {
+			super(numAg, numConta, saldo);
+			this.nome = nome;
+			this.cpf = cpf;
+		}
+
+		@Override
+		public void consultarDados() {
+			System.out.println("Nome: " + nome);
+			System.out.println("CPF: " + cpf);
+		}
+
+		@Override
+		public void consultarSaldo() {
+			System.out.println("Saldo: " + saldo);
+		}
+
+		@Override
+		public void depositar(double valor) {
+			saldo += valor;
+			System.out.println("Depósito realizado com sucesso!");
+		}
+
+		@Override
+		public void sacar(double valor) {
+			if (saldo >= valor) {
+				saldo -= valor;
+				System.out.println("Saque realizado com sucesso!");
+			} else {
+				System.out.println("Saldo insuficiente para saque!");
+			}
+		}
+	}
+}*/
+
+ //  COPILOT
+
+package com.banco;
+
+import javax.swing.*;
+		import java.util.Random;
+
+// A classe BancoAPP não deve estender ContaPF a menos que haja uma relação de "é um" entre eles.
+// Se BancoAPP é uma ContaPF, então a herança faz sentido. Caso contrário, é melhor usar composição.
+public class BancoAPP {
+	// Removido o construtor que estava chamando super com valores fixos.
+
+	public static void main(String[] args) {
+		// As variáveis não utilizadas foram removidas.
+		Object[] menu = {"Login", "Novo Usuario", "sair"};
+		int opcao;
+
+		// Instâncias não utilizadas foram removidas.
+		Random G = new Random();
+		Pessoa User = new Pessoa(01);
+		Form Tela = new Form();
+
+		// Menu inicial
+		opcao = JOptionPane.showOptionDialog(null,
+				"Escolha uma opção",
+				"SeuBank",
+				JOptionPane.YES_NO_CANCEL_OPTION,
+				JOptionPane.QUESTION_MESSAGE,
+				null,
+				menu,
+				menu[0]);
+
+		switch (opcao) {
+			case 0:
+				SwingUtilities.invokeLater(() -> {
+					Tela.setVisible(true);
+					Tela.setSize(750, 600);
+					Tela.setLocationRelativeTo(null);
+					Tela.setTitle("My Bank");
+					JLabel mensagem = new JLabel("Bem vindo, a tela de Login:");
+					Tela.getContentPane().add(mensagem);
+					Tela.pack();
+				});
+				break;
+
+			case 1:
+				SwingUtilities.invokeLater(() -> {
+					Tela.setVisible(true);
+					Tela.setSize(750, 600);
+					Tela.setLocationRelativeTo(null);
+					Tela.setTitle("My Bank");
+					JLabel mensagem = new JLabel("Bem vindo, a tela de Novo usuario:");
+					Tela.getContentPane().add(mensagem);
+					Tela.pack();
+				});
+				break;
+			case 2:
+				Tela.setVisible(true);
+				Tela.setSize(750, 600);
+				Tela.setTitle("My Bank");
+				JLabel mensagem = new JLabel("Volte sempre!:");
+				Tela.getContentPane().add(mensagem);
+				Tela.pack();
+				System.out.println("volte sempre");
+				break;
+		}
 	}
 }
-
-
-
-
-			/*Tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			Tela.add(inicializar); // adiciona o painel ao frame ÿ
-			Tela.setSize(250, 250); // configura o tamanho do frameÿ
-			Tela.setVisible(true); // torna o frame visível*/
-
-					/*opcao = JOptionPane.showInputDialog
-					(null, "--- Menu --- :", "tipo: ",
-							JOptionPane.PLAIN_MESSAGE, null,menu, menu[0]));
-		});*/
-
-
-
-		//System.out.println(opcao);
-		 /*switch (opcao) {
-			 case "Login":
-				 User.setNome(JOptionPane.showInputDialog("Digite seu nome: "));
-			break;
-			 case "Novo Usuario":
-				 JOptionPane.showInputDialog(null, "Digite seu nome: ", "tipo: ", JOptionPane.PLAIN_MESSAGE, null,opcoes, opcoes[0]);
-		 	break;
-			 case "Sair":
-				 //JOptionPane.show();
-			 break;
-		 }
-
-		 opcao = JOptionPane.showInputDialog(null, "Selecione o tipo de conta :", "tipo: ", JOptionPane.PLAIN_MESSAGE, null,opcoes, opcoes[0]);
-    */
-//input data in pessoa fisica
-
-
-
-	/*if(opcao == "Conta PF") {
-//instance class ContaPF
-    	ContaPF cc = new ContaPF(0,0,0.0);
-    	
-    	cc.setNumAG(10);
-    	cc.setNumConta(G.nextInt(10));
-    	cc.setSaldo(G.nextDouble(0.0));
-    	JOptionPane.showMessageDialog(null, "Vamos iniciar a coleta de dados de usuário");
-	    
-//instancias dentro do PF	    
-	    Pessoa usuario = new Pessoa(G.nextInt(100));
-	    PessoaFisica userF = new PessoaFisica();
-	    
-//get dates of pessoa fisica
-	    usuario.setNome(JOptionPane.showInputDialog("Digite seu nome"));
-	    userF.setCpf(JOptionPane.showInputDialog("Digite seu CPF: "));
-	    userF.setRg(JOptionPane.showInputDialog("Digite seu RG: "));
-	     
-//Definindo salarios aos cargos
-	    userF.setCargo(JOptionPane.showInputDialog(null, "Selecione o seu cargo:", "Menu de Cargos", JOptionPane.PLAIN_MESSAGE, null, cargos, cargos[0]));
-	    if(userF.getCargo() == "Programador") {
-	    	userF.setSalario(5000);
-	    	}
-	    else if (userF.getCargo() == "Gerente") {
-	    	userF.setSalario(10000);
-	    	}
-	    else if (userF.getCargo() == "Coordenador") {
-	    	userF.setSalario(20000);
-	    	}
-	    else {
-	    	userF.setSalario(3000);
-	    	}
-	    
-//pessoa fisica chose what want to do with your count
-	   do { oppf = JOptionPane.showInputDialog(null, "Selecione a operação", "Operações", JOptionPane.PLAIN_MESSAGE, null, opspf, opspf[0]);
-	   
-//add references of other class
-		   //UserF = usuario.setNome();
-		   //usuario.n = userF;
-	    
-	    if (oppf == "Consultar Dados") {
-	    	JOptionPane.showMessageDialog(null, "CPF: " + usuario.getNome());
-	    }
-	    //else if (oppf == "Consultar Saldo") {
-	    //	JOptionPane.showMessageDialog(null, "Saldo: "+ usuario.getSalario());
-	    //}
-	    } while (oppf != "Sair");
-    }
-
-    
-//Caso a conta seja de pessoa juridica 
-    else if (opcao == "Conta PJ") {
-    		empresa = JOptionPane.showInputDialog(null, "Selecione seu tipo de Empresa", "Menu de Tipos", JOptionPane.PLAIN_MESSAGE, null, empresas, empresas[0]);
-    		
-    		PessoaJuridica user = new PessoaJuridica(G.nextInt(), empresa);
-    		user.setCnpj(JOptionPane.showInputDialog("Digite seu CNPJ: "));
-    		user.setInscricao(JOptionPane.showInputDialog(null, "Informe sua inscrição :"));
-    		user.setSite(JOptionPane.showInputDialog(null, "Informe seu site :"));
-    		
-    	} 
-
-		*/
 
 
